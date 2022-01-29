875df8a42819d87de21ddd522a72984c984b7899
Shubham <nhaldes@gmail.com>
2022-01-29 10:24:33 +0530

    Added: Harmonic Number

---
+++ HarmonicNumber.java
@@ -0,0 +1,28 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class HarmonicNumber {
+	
+    public static void main(String[] args){
+
+        int N = nthValue();
+        double nthHarmonic = 0;
+
+        while (N==0){
+            System.out.println("Enter a value other than zero.");
+            N = nthValue();
+        }
+
+        for(int i=1;i<=N;i++){
+            nthHarmonic+=(double)1/i;
+            System.out.println(i+" th Harmonic Number is 1/"+i+" = "+ nthHarmonic);
+        }
+    }
+    public static int nthValue(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the value of N: ");
+        return scanner.nextInt();
+    }
+
+}
