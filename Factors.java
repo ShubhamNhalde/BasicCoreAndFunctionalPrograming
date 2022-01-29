6c99676d4489a77cb130ffeb4a0e5d6572e9feb2
Shubham <nhaldes@gmail.com>
2022-01-29 10:25:27 +0530

    Added: Factors

---
+++ Factors.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class Factors {
+
+	public static void main(String[] args){
+
+		int number;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a number ::");
+		number = scanner.nextInt();
+		System.out.println("Prime factors are: ");
+
+		for(int i = 2; i<= number; i++) {
+			while(number%i == 0) {
+				System.out.println(i);
+				number = number/i;
+			}
+		}
+	}
+
+}
